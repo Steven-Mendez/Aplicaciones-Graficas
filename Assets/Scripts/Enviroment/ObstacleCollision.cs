e9dc79aee0b4c640140a4c449fa1cf77276d4d9e
Steveb Mendez <Stevenampaiz@gmail.com>
2023-05-06 16:50:01 -0600

    ENEMY OBSTACLES

--- Assets/Scripts/Enviroment/ObstacleCollision.cs
+++ Assets/Scripts/Enviroment/ObstacleCollision.cs
@@ -4,11 +4,15 @@ public class ObstacleCollision : MonoBehaviour
 {
     public GameObject thePlayer;
     public GameObject charModel;
+    public AudioSource crashThud;
+    public GameObject mainCam;
 
     private void OnTriggerEnter(Collider other)
     {
         gameObject.GetComponent<BoxCollider>().enabled = false;
         thePlayer.GetComponent<PlayerMove>().enabled = false;
         charModel.GetComponent<Animator>().Play("Stumble Backwards");
+        crashThud.Play();
+        mainCam.GetComponent<Animator>().Play("CamShake 0");
     }
 }
