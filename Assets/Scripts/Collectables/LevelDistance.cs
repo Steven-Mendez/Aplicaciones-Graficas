3b6151e4b48508680a79c801357c862763fc7af0
Steveb Mendez <Stevenampaiz@gmail.com>
2023-05-07 15:00:55 -0600

    Transitions

--- Assets/Scripts/Collectables/LevelDistance.cs
+++ Assets/Scripts/Collectables/LevelDistance.cs
@@ -10,6 +10,11 @@ public class LevelDistance : MonoBehaviour
     public bool addingDis = false;
     public float disDelay = 0.35F;
 
+    private void Start()
+    {
+        StartCoroutine(AddSpeed());
+    }
+
     void Update()
     {
         if (!addingDis)
@@ -26,5 +31,14 @@ public class LevelDistance : MonoBehaviour
         disEndDisplay.GetComponent<Text>().text = disRun.ToString();
         yield return new WaitForSeconds(disDelay);
         addingDis = false;
+    }
+
+    IEnumerator AddSpeed()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5);
+            PlayerMove.moveSpeed *= 1.1f;
+        }
     }
 }
--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -1,10 +1,19 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float moveSpeed = 5;
-    public float leftRigthSpeed = 4;
+    public static float moveSpeed = 5.5f;
+    public static float leftRigthSpeed = moveSpeed - 1;
     static public bool canMoove = false;
+    public bool isJumping = false;
+    public bool comingDown = false;
+    public GameObject playerObject;
+
+    private void Start()
+    {
+        moveSpeed = 5.5f;
+    }
 
     void Update()
     {
@@ -28,5 +37,57 @@ public class PlayerMove : MonoBehaviour
                 transform.Translate(leftRigthSpeed * Time.deltaTime * Vector3.right, Space.World);
             }
         }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Space))
+        {
+            if (!isJumping)
+            {
+                playerObject.GetComponent<Animator>().Play("Jump");
+                isJumping = true;
+                StartCoroutine(JumpSequence());
+            }
+        }
+
+        if (isJumping)
+        {
+            if (!comingDown)
+            {
+                transform.Translate(3.5f * Time.deltaTime * Vector3.up, Space.World);
+            }
+            if (comingDown)
+            {
+                transform.Translate(-3.5f * Time.deltaTime * Vector3.up, Space.World);
+            }
+        }
     }
+
+    IEnumerator JumpSequence()
+    {
+        float initialYPosition = transform.position.y; // Guardar la posición inicial en el eje Y
+        float jumpHeight = 2f; // Altura del salto
+        float timeToReachPeak = 0.2f; // Tiempo para alcanzar la altura máxima del salto
+
+        float timeElapsed = 0f;
+        float yOffset = 0f;
+
+        while (timeElapsed < timeToReachPeak)
+        {
+            yOffset = Mathf.Lerp(0f, jumpHeight, timeElapsed / timeToReachPeak); // Calcular el desplazamiento vertical en función del tiempo
+            transform.position = new Vector3(transform.position.x, initialYPosition + yOffset, transform.position.z);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        while (yOffset > 0f)
+        {
+            yOffset -= 3.5f * Time.deltaTime; // Descender gradualmente
+            transform.position = new Vector3(transform.position.x, initialYPosition + yOffset, transform.position.z);
+            yield return null;
+        }
+
+        transform.position = new Vector3(transform.position.x, initialYPosition, transform.position.z); // Establecer la posición Y en la original
+        isJumping = false;
+        playerObject.GetComponent<Animator>().Play("Standard Run");
+    }
+
 }
